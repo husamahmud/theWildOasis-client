552c176fdca3dabdf57a2924d3446b883788467b
husamahmud <husamahmud@gmail.com>
2024-07-27 20:38:04 +0300

    feat: Implement home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,33 +7,31 @@ import '@/app/_styles/globals.css'
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
-  display: 'swap',
+  display: 'swap'
 })
 
 export const metadata: Metadata = {
   title: {
     template: '%s / The Wild Oasis',
-    default: 'Welcome / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis'
   },
-  description: 'A place to relax and enjoy the great outdoors.',
+  description: 'A place to relax and enjoy the great outdoors.'
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout({ children }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100`}
-      >
-        <Header />
+    <body
+      className={`${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100`}
+    >
+    <Header />
 
-        <div className="flex-1 px-8 py-12">
-          <main className="mx-auto max-w-7xl">{children}</main>
-        </div>
-      </body>
+    <div className="flex-1 px-8 py-12">
+      <main className="mx-auto max-w-7xl">{children}</main>
+    </div>
+    </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+import Image from 'next/image'
+import bg from 'public/bg.png'
+import Link from 'next/link'
+
 export default function Page() {
   return (
-    <p>Welcome to the Wild Oasis</p>
+    <div className="mt-24">
+      <Image src={bg}
+             fill
+             quality={100}
+             placeholder="blur" // only works with next/image and statically imported images
+             className="object-cover object-top"
+             alt="Mountains and forests with two cabins" />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg
+        font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </div>
   )
 }
