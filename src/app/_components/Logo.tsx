061982612c79892c67fba2d4c3621d281b897ea3
husamahmud <husamahmud@gmail.com>
2024-07-27 20:25:03 +0300

    enhance: Use `Image` instead of img tag

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 function Logo() {
   return (
     <Link href="/"
           className="flex items-center gap-4 z-10">
-      <img
+      <Image
+        src="/logo.png"
+        quality={100}
         height="60"
         width="60"
-        src="/logo.png"
         alt="The Wild Oasis logo"
       />
       <span className="text-xl font-semibold text-primary-100">
