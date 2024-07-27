b7eb21cc0e11a93136b14e4e0b8170f456a5ef7b
husamahmud <husamahmud@gmail.com>
2024-07-27 20:52:44 +0300

    feat: Implement About & Cabins page

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,19 +1,35 @@
-import Counter from '@/app/Counter'
+import CabinCard from '@/app/_components/CabinCard'
 
 export const metadata = {
   title: 'Cabins Area',
-  description: 'View all available cabins.',
+  description: 'View all available cabins.'
 }
 
 export default async function Page() {
-  await new Promise((resolve) => setTimeout(resolve, 2000)) // 2-second delay
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
-  const data = await response.json()
+  const cabins: any = []
 
   return (
     <div>
-      <h1>cabins</h1>
-      <Counter data={data} />
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      {cabins.length > 0 && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin: any) => (
+            <CabinCard cabin={cabin}
+                       key={cabin.id} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
