c5b0dc412c761f587caee2ab0216f6c4bfabf0e3
husamahmud <husamahmud@gmail.com>
2024-08-06 23:35:56 +0300

    feat: Configured `nextAuth`

--- src/app/_components/Navigation.tsx
+++ src/app/_components/Navigation.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
+import { auth } from '@/app/_lib/auth'
+import Image from 'next/image'
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth()
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex items-center gap-16">
@@ -14,11 +18,29 @@ export default function Navigation() {
             About
           </Link>
         </li>
-        <li>
-          <Link href="/account" className="transition-colors hover:text-accent-400">
+        {session?.user?.image ? (
+          <li>
+            <Link
+              href="/account"
+              className="flex items-center gap-3 transition-colors hover:text-accent-400">
+              <Image
+                src={session.user.image}
+                alt="profile picture"
+                className="rounded-full"
+                width={25}
+                height={25}
+                referrerPolicy="no-referrer"
+              />
+              Guest area
+            </Link>
+          </li>
+        ) : (
+          <Link
+            href="/account"
+            className="flex items-center gap-3 transition-colors hover:text-accent-400">
             Guest area
           </Link>
-        </li>
+        )}
       </ul>
     </nav>
   )
